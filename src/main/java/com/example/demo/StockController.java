7de090412fccd5bc48a36bb412191d65f653f94b
prabhleen9kaur <prabhleen09kaur@gmail.com>
2019-08-18 12:51:51 -0400

    This is my Second commit.These files contains the source code of demo project.
    This repository contains Project on Banking System (Portfolio Analysis)

---
+++ src/main/java/com/example/demo/StockController.java
@@ -0,0 +1,134 @@
+package com.example.demo;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.StockTransaction.State;
+
+@RestController
+public class StockController {
+
+	private StockTransactionRepository stockTransactionRepository;
+	private RealizedStockRepository realizedStockRepository;
+	private  SymbolRepository symbolRepository;
+	private  StockService stockService;
+
+
+	public StockController(StockTransactionRepository stockTransactionRepository,
+			RealizedStockRepository realizedStockRepository, SymbolRepository symbolRepository,
+			StockService stockService) {
+		this.stockTransactionRepository = stockTransactionRepository;
+		this.realizedStockRepository = realizedStockRepository;
+		this.symbolRepository = symbolRepository;
+		this.stockService = stockService;
+	}
+
+	@GetMapping("/current")
+	public List<StockTotalObject> currentStockInfo() {
+		List<StockTransaction> stockList = stockTransactionRepository.findAllByOrderBySymbolAsc();
+		List<StockTotalObject> stockTotalObjectList = stockService.getStockInfo(stockList);
+
+		return stockTotalObjectList;
+	}
+	
+	@GetMapping("/profits")
+	public String profits() {
+		
+		List<StockTransaction> stockList = stockTransactionRepository.findAllByOrderBySymbolAsc();
+		List<StockTotalObject> stockTotalObjectList = stockService.getStockInfo(stockList);
+		
+		String profit = stockService.getTotalProfit(stockTotalObjectList);
+
+		return profit;
+	}
+
+	@GetMapping("/realized")
+	public List<StockRealized> realizedProfits() {
+		List<StockRealized> realizedList = realizedStockRepository.findAll();
+		return realizedList;
+
+	}
+
+	@GetMapping("/transactions")
+	public List<StockTransaction> transactions() {
+		List<StockTransaction> transactions = stockTransactionRepository.findAllByOrderBySymbolAsc();
+		return transactions;
+
+	}
+
+	@PostMapping("/post")
+	public ResponseEntity<?> post(@Valid @RequestBody StockTransaction st, Errors errors){
+		
+
+		if (errors.hasErrors() || stockService.getCurrentPrice(st.getSymbol()).compareTo(BigDecimal.ZERO) == 0 
+				|| st.getNumOfShares() == 0) {
+			return ResponseEntity.badRequest().body("Bad Request");
+		}
+
+		st.setSymbol(st.getSymbol().toUpperCase());
+
+		if (st.getState().name().equals("PURCHASE"))
+			st.setSharesInLot(st.getNumOfShares());
+		
+		if (st.getState().name().equals("SALE")){
+			stockService.calcRealizedProfit(st);
+		}
+
+
+		return ResponseEntity.ok(stockTransactionRepository.save(st));
+
+
+	}
+	
+	@GetMapping("/sample")
+	public ResponseEntity<?> sample(){
+		
+		String[] array = {"AAPL", "TWTR", "NFLX", "MSFT", "GOOG", "TSLA", "AMD", "NVDA", "FB", "UBER", "GE", "AMZN"};
+				
+		String random = array[(int) (Math.random() * array.length)];
+		
+		StockTransaction st = new StockTransaction();
+		st.setSymbol(random);
+		st.setNumOfShares(100);
+		st.setSharesInLot(st.getNumOfShares());
+		st.setState(State.PURCHASE);
+		st.setPriceOfShares(stockService.getCurrentPrice(random));
+		
+
+		return ResponseEntity.ok(stockTransactionRepository.save(st));
+
+	}
+	
+	@GetMapping("/samplesale")
+	public ResponseEntity<StockTransaction> sampleSale(){
+		
+		String[] array = {"AAPL", "MSFT", "FB", "GOOG"};
+				
+		String random = array[(int) (Math.random() * array.length)];
+		
+		StockTransaction st = new StockTransaction();
+		st.setSymbol(random);
+		st.setNumOfShares(100);
+		st.setPriceOfShares(stockService.getCurrentPrice(random));
+		st.setState(State.SALE);
+
+				
+		stockService.calcRealizedProfit(st);
+		
+		return ResponseEntity.ok(stockTransactionRepository.save(st));
+
+
+
+	}
+
+}
